9cea3b1c13bb4f80135ddda2278520cbcea59bd1
Asiwajuife <101390789+Asiwajuife@users.noreply.github.com>
2022-07-17 12:53:06 -0700

    Update 1-isdigit.c
    xdj

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,19 +1,20 @@
+/*
+* Auth: asiwaju
+*/
+
 #include "main.h"
 
 /**
- * _isdigit - checks for digits
- * @c: parameter
- * Return: 0 or 1
+ * _isdigit - Checks for a digit (0-9).
+ * @c: The number to be checked.
+ *
+ * Return: 1 if the number is a digit, 0 otherwise.
  */
-
 int _isdigit(int c)
 {
-	if (c >= 48 && c < 58)
-	{
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
+
 	else
-	{
 		return (0);
-	}
 }
