c5ea8199c59953e4ab0effe5a02b2196be4f0ff6
Asiwajuife <101390789+Asiwajuife@users.noreply.github.com>
2022-07-17 15:35:33 -0500

    Jhb

--- 0x18-dynamic_libraries/1-strncat.c
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -1,32 +1,28 @@
 #include "main.h"
+
 /**
- *_strncat - concatenates two strings
- *@dest: A pointer to a character that will be changed
- *@src: A pointer to a character that will be changed
- *@n: value
- *Return: dest
+ * _strncat - concatenate n bytes to destination string
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes(chars)
+ * Return: concatenated string
  */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
-int i, j;
+	int i = 0;
+	int j = 0;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
+	while (dest[i] != '\0') /*i is index of null terminator*/
+		i++;
 
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
+	while (src[j] != src[n]) /*append replacing null terminator*/
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 
-dest[i] = '\0';
-
-return (dest);
+	return (dest);
 }
