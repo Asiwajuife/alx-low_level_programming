263417f929434ce2a461aa8727521a30955f863d
Asiwajuife <101390789+Asiwajuife@users.noreply.github.com>
2022-07-17 15:10:41 -0500

    Hi

--- 0x18-dynamic_libraries/2-strncpy.c
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -1,30 +1,24 @@
 #include "main.h"
+
 /**
- *_strncpy - copies a string
- *@dest: A pointer to a character that will be changed
- *@src: A pointer to a character that will be changed
- *@n: value
- *Return: dest
+ * _strncpy - copies n bytes of a source into buffer of a destination string,
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * @n: n bytes to be copied
+ * Return: copied string
  */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
-int i;
+	int i;
 
-i = 0;
-while (i < n && src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
-}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
-
-return (dest);
-
+	return (dest);
 }
